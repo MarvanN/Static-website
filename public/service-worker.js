084c77b83d047256ac3f1af9fc7d867ecfd53674
MarvanN <marvann619@gmail.com>
2024-08-12 16:08:34 +0530

    added service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,45 @@
+// service-worker.js
+
+const CACHE_NAME = 'my-cache-v1';
+const CACHE_ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/favicon.ico',
+    '/logo192.png',
+    '/logo512.png',
+    '/static/css/main.css', // Update with your CSS path
+    '/static/js/main.js',   // Update with your JS path
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Caching files');
+            return cache.addAll(CACHE_ASSETS);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
